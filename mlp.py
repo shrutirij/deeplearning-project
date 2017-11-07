7f6d9900ecfa0800ee86d230bcc63ab0909e5aa7
samridhishree <sami@samridhishree.com>
2017-11-07 18:00:23 -0500

    Adding the MLP class

---
+++ mlp.py
@@ -0,0 +1,93 @@
+'''
+Multi-Layer Perceptron in DyNet.
+Contains separate classes for Layers and the model to make it generic.
+'''
+
+import dynet as dy
+import numpy as np
+
+'''
+Class definition for a layer in MLP with the weights and the bias parameters.
+'''
+class Layer(object):
+	def __init__(self, input_dim, output_dim):
+		'''
+		input_dim: Input dimension of the layer
+		output_dim: Output dimension of the layer
+		activation: Activation function used for the layer (tanh, rectify, logistic)
+		drop_rate: dropout rate if any
+		'''
+		self.input_dim = input_dim
+		self.output_dim = output_dim
+
+
+'''
+Creates layers according to given dimensions and returns a list
+'''
+def create_layers(num_layers, layer_dims):
+	layer_list = []
+	for l in range(num_layers):
+		temp_l = Layer(layer_dims[l][0], layer_dims[l][1])
+		layer_list.append(temp_l)
+	return layer_list
+
+'''
+Creates and returns dynet params for each layer object for the model.
+Assumption - the input param has been initialized with proper dimensions.
+Return: the updated model and the list of parameter objects
+'''
+def create_params(model, layers):
+	param_list = []
+	for l in range(len(layers)):
+		Parameter W = model.add_parameters((layer.input_dim, layer.output_dim))
+		Parameter b = model.add_parameters((layer.output_dim))
+		param_list.append((W, b))
+	return model, param_list
+
+'''
+Class Definition for MLP
+'''
+class MLP(object):
+	def __init__(self, num_layers, layer_dims, activation, drop_rate):
+		'''
+		num_layers: number of layers in the network
+		layer_dims: List of layer dimension tuples [(i1,h1), (h1,h2), (h2,h3)...]
+		activation: Activation function used for the layer (tanh, rectify, logistic)
+		drop_rate: dropout rate if any
+		'''
+		self.num_layers = num_layers
+		self.layer_dims = layer_dims
+		self.activation = activation
+		self.drop_rate = drop_rate
+		self.model = dy.Model()
+		self.layers = create_layers(num_layers, layer_dims)
+		self.model, self.params = create_params(model, layers)
+
+	def forward(input_expr):
+		cg = dy.renew_cg()
+		h_cur = input_expr
+
+		# Feed forward through all but the last layer
+		L = self.num_layers
+		for l in range(L-1):
+			w_cur = dy.parameter(self.params[l][0])
+			b_cur = dy.parameter(self.params[l][1])
+			act = self.activation
+			h_new = act((w_cur * h_cur) + b_cur)
+			h_cur = h_new
+
+		# Return logistic sigmoid for binary classification for the last layer
+		w_last = dy.parameter(self.params[L-1][0])
+		b_last = dy.parameter(self.params[L-1][1])
+		y_pred = dy.logistic((w_last * h_cur) + b_last)
+		return y_pred
+
+
+
+
+
+
+
+
+
+
