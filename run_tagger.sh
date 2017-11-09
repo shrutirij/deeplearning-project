ce88e5c27893fce5c2672edcae493731758ed297
Shruti Rijhwani <shruti@Shrutis-MBP.wv.cc.cmu.edu>
2017-11-09 13:49:23 -0500

    sbatch update

--- run_tagger.sh
+++ run_tagger.sh
@@ -4,5 +4,7 @@
 #SBATCH -t 0
 
 # Training
+module load singularity
+singularity shell /projects/tir1/singularity/ubuntu-16.04-lts_tensorflow-1.4.0_cudnn-8.0-v6.0.img
 python bilstm.py --dynet-gpu --dynet-mem 11500 --dynet-autobatch 1
 
