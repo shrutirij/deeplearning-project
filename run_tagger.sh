5f99501a282787ee702df2e5b9d0d0ee1b178879
Shruti Rijhwani <shruti@Shrutis-MBP.wv.cc.cmu.edu>
2017-11-09 14:38:43 -0500

    sbatch run change

--- run_tagger.sh
+++ run_tagger.sh
@@ -2,9 +2,10 @@
 #SBATCH -c 1
 #SBATCH --mem=15g
 #SBATCH -t 0
+#SBATCH --gres=gpu:1
+
+source ~/.bashrc
 
 # Training
-module load singularity
-singularity shell /projects/tir1/singularity/ubuntu-16.04-lts_tensorflow-1.4.0_cudnn-8.0-v6.0.img
-python bilstm.py --dynet-gpu --dynet-mem 11500 --dynet-autobatch 1
-
+source activate dynetenv
+python bilstm.py --dynet-gpu --dynet-mem 11000 --dynet-autobatch 1
