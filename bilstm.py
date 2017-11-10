a9b2608346a77aac868532626e2cebeab0b247b0
Shruti Rijhwani <shruti@Shrutis-Macbook-Pro.local>
2017-11-09 22:00:35 -0500

    change to word embeds

--- bilstm.py
+++ bilstm.py
@@ -8,31 +8,30 @@ BATCH_SIZE = 32
 UNK = '$unk'
 
 class BiLSTMTagger(object):
-    def __init__(self, embed_size, char_hidden_size, word_hidden_size, training_file, dev_file, test_file):
-        self.training_data, self.char_vocab, self.tag_vocab = self.read(training_file)
+    def __init__(self, embed_size, word_hidden_size, mlp_layer_size, training_file, dev_file, test_file):
+        self.training_data, self.word_vocab, self.tag_vocab = self.read(training_file)
         self.tag_lookup = dict((v,k) for k,v in self.tag_vocab.iteritems())
         self.dev_data = self.read_unk(dev_file)
         self.test_data = self.read_unk(test_file)
 
         self.model = dy.Model()
 
-        self.char_embeds = self.model.add_lookup_parameters((len(self.char_vocab), embed_size))
-        self.char_lstm = dy.BiRNNBuilder(1, embed_size, char_hidden_size, self.model, dy.LSTMBuilder)
-        self.word_lstm = dy.BiRNNBuilder(1, char_hidden_size, word_hidden_size, self.model, dy.LSTMBuilder)
-        self.feedforward = mlp.MLP(self.model, 2, [(word_hidden_size,128),(128,len(self.tag_vocab))], 'sigmoid', 0.0)
+        self.word_embeds = self.model.add_lookup_parameters((len(self.word_vocab), embed_size))
+        self.word_lstm = dy.BiRNNBuilder(1, embed_size, word_hidden_size, self.model, dy.LSTMBuilder)
+        self.feedforward = mlp.MLP(self.model, 2, [(word_hidden_size,mlp_layer_size), (mlp_layer_size,len(self.tag_vocab))], 'tanh', 0.0)
 
     def read(self, filename):
         train_sents = []
-        char_vocab = defaultdict(lambda: len(char_vocab))
+        word_vocab = defaultdict(lambda: len(word_vocab))
         tags = defaultdict(lambda: len(tags))
 
         with codecs.open(filename, 'r', 'utf8') as fh:
             for line in fh:
                 line = line.strip().split()
                 sent = [tuple(x.rsplit("/",1)) for x in line]
-                sent = [([char_vocab[c] for c in word], tags[tag]) for word, tag in sent]
+                sent = [(word_vocab[word], tags[tag]) for word, tag in sent]
                 train_sents.append(sent)
-        return train_sents, char_vocab, tags
+        return train_sents, word_vocab, tags
 
     def read_unk(self, filename):
         sents = []
@@ -41,15 +40,15 @@ class BiLSTMTagger(object):
             for line in f:
                 line = line.strip().split()
                 sent = [tuple(x.rsplit("/",1)) for x in line]
-                sent = [([self.char_to_int(c) for c in word], self.tag_vocab[tag]) for word, tag in sent]
+                sent = [(self.word_to_int(word), self.tag_vocab[tag]) for word, tag in sent]
                 sents.append(sent)
         return sents
 
-    def char_to_int(self, char):
-        if char in self.char_vocab:
-            return self.char_vocab[char]
+    def word_to_int(self, word):
+        if word in self.word_vocab:
+            return self.word_vocab[word]
         else:
-            return self.char_vocab[UNK]
+            return self.word_vocab[UNK]
 
     def lookup_tag(self, tag_id):
         return self.tag_lookup[tag_id]
@@ -60,8 +59,7 @@ class BiLSTMTagger(object):
         for sent in sents:
             dy.renew_cg()
             cur_tags = []
-            word_reps = [self.char_lstm.transduce([self.char_embeds[c] for c in word])[-1] for word, tag in sent]
-            contexts = self.word_lstm.transduce(word_reps)
+            contexts = self.word_lstm.transduce([self.word_embeds[word] for word, tag in sent])
             for context in contexts:
                 probs = dy.softmax(self.feedforward.forward(context)).vec_value()
                 pred_tag = probs.index(max(probs))
@@ -75,8 +73,7 @@ class BiLSTMTagger(object):
         words = 0
 
         for sent in sents:
-            word_reps = [self.char_lstm.transduce([self.char_embeds[c] for c in word])[-1] for word, tag in sent]
-            contexts = self.word_lstm.transduce(word_reps)
+            contexts = self.word_lstm.transduce([self.word_embeds[word] for word, tag in sent])
             probs = dy.concatenate_to_batch([self.feedforward.forward(context) for context in contexts])     
             cur_losses = dy.pickneglogsoftmax_batch(probs, [tag for word,tag in sent])
             losses.append(dy.sum_batches(cur_losses)) 
@@ -98,7 +95,7 @@ class BiLSTMTagger(object):
                 trainer.update()
                 num_batches += 1
 
-                if num_batches % 50 == 0:
+                if num_batches % 1 == 0:
                     print('Validation loss: %f' % self.get_loss(self.dev_data))
                     print('Validation accuracy: %f' % self.get_accuracy(self.dev_data))
                     print('Test accuracy: %f' % self.get_accuracy(self.test_data))
@@ -124,6 +121,8 @@ class BiLSTMTagger(object):
 
 
 if __name__ == '__main__':
-    tagger_model = BiLSTMTagger(128, 128, 256, './data/train_data_pruned.txt','./data/dev_data.txt','./data/test_data.txt')
+    tagger_model = BiLSTMTagger(256, 256, 128, './data/train_data_pruned.txt','./data/dev_data.txt','./data/test_data.txt')
     tagger_model.train(1000)
+    # tagger_model = BiLSTMTagger(16, 16, 8, './data/small_data.txt','./data/small_data.txt','./data/small_data.txt')
+    # tagger_model.train(100)
     
