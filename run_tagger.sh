932bfcbe2936938246153e6e536d7493a0c19d12
Shruti Rijhwani <shruti@Shrutis-Macbook-Pro.local>
2017-11-09 22:19:23 -0500

    change batch script

--- run_tagger.sh
+++ run_tagger.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 #SBATCH -c 1
-#SBATCH --mem=15g
+#SBATCH --mem=15000
 #SBATCH -t 0
-#SBATCH --nodelist=compute-0-19
 #SBATCH --gres=gpu:1
 
 source ~/.bashrc
-export LD_LIBRARY_PATH=/projects/tir2/users/srijhwan/dynet-base/dynet/build/dynet/:$LD_LIBRARY_PATH
 
 # Training
-module load singularity
-singularity shell --nv /projects/tir1/singularity/ubuntu-16.04-lts_tensorflow-1.4.0_cudnn-8.0-v6.0.img
-source activate dynetenv
-python bilstm.py --dynet-gpu --dynet-mem 11000 --dynet-autobatch 1
+python fixed_neurons.py --dynet-gpu --dynet-mem 10000 --dynet-autobatch 1
